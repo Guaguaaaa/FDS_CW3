3ac8238b8dfe525c55da673a2f2d05c11686e704
Guaguaaaa <guanyinghao223@gmail.com>
2024-03-31 00:02:27 +0000

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print("hello world!")
+import numpy as np
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+dtypes = {
+    #'Year of event': int,
+    #'Event dates': 'datetime',
+    #'Event name': str,
+    'Event distance/length': object,
+    #'Event number of finishers': int,
+    'Athlete performance': object,
+    #'Athlete club': str,
+    #'Athlete country': str,
+    #'Athlete year of birth': int,
+    #'Athlete gender': 'category',
+    #'Athlete age category': str,
+    'Athlete average speed': object,  # has data like '18:00:00' in it.
+    #'Athlete ID': int
+}
+
+# 读取CSV文件
+df = pd.read_csv("TWO_CENTURIES_OF_UM_RACES.csv", dtype=dtypes)
+
+# change date
+df['Event dates'] = pd.to_datetime(df['Event dates'], errors='coerce', format='%d.%m.%Y', dayfirst=True)
+
+
+# delete exceptions in date
+df = df.dropna(subset=['Event dates'])
+
+# convert time to seconds
+# df['Athlete performance'] = pd.to_timedelta(df['Athlete performance']).dt.total_seconds() / 3600
+
+# participles and year
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df.groupby('Year of event')['Event number of finishers'].sum())
+plt.title('Number of Finishers Over Years')
+plt.xlabel('Year')
+plt.ylabel('Number of Finishers')
+plt.show()
