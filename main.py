4339365e726ad5641632d87885aed5f98c07a250
Guaguaaaa <guanyinghao223@gmail.com>
2024-04-01 10:56:44 +0100

    comment update

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+
 dtypes = {
     'Year of event': 'int64',
-    'Event dates': object,
+    'Event dates': object,  # there are 00.00.1995, or 05.-07.01.2018
     'Event name': object,
     'Event distance/length': object,
     'Event number of finishers': 'int64',
     'Athlete performance': object,
     'Athlete club': object,
     'Athlete country': object,
-    'Athlete year of birth': 'float64',
-    'Athlete gender': object,
+    'Athlete year of birth': 'float64',  # contains 1978.0 (always .0)
+    'Athlete gender': object,  # Gender: F or M or X
     'Athlete age category': object,
     'Athlete average speed': object,  # has data like '18:00:00' in it.
     'Athlete ID': 'int64'
@@ -26,6 +27,11 @@ df = pd.read_csv("TWO_CENTURIES_OF_UM_RACES.csv", dtype=dtypes)
 
 # change date
 df['Event dates'] = pd.to_datetime(df['Event dates'], errors='coerce', format='%d.%m.%Y', dayfirst=True)
+
+# clean 'Athlete year of birth' data, convert to int type (all 4 digit int of year)
+df['Athlete year of birth'] = df['Athlete year of birth'].astype(str).str.replace('.0', '')
+# df['Athlete year of birth'] = df['Athlete year of birth'].astype(int)
+df['Athlete year of birth'] = pd.to_numeric(df['Athlete year of birth'], errors='coerce').astype('Int64')
 
 
 # delete exceptions in date
